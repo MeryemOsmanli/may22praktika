b2efb545c6009c8ab2e641e453dd5ddf44dbb640
Em Sol <bd825jfhe@code.edu.az>
2024-05-31 18:00:10 +0400

    login and register introduction

--- back/src/controllers/UserController.js
+++ back/src/controllers/UserController.js
@@ -1,8 +1,49 @@
 import { May22Blog } from "../models/UserModel.js"
+import bcrypt from 'bcrypt'
+import  jwt  from "jsonwebtoken"
+const tokenKey="salam1243"
 
 export const userGetAll= async (req, res) => {
-    const getAll = await May22Blog.find()
-    res.send(getAll)
+  try {
+    const token=req.headers.authorization
+    const decoded= jwt.verify(token,tokenKey)
+    if (decoded.role==="Admin") {
+        const user=await May22Blog.find()
+        return res.send(user)
+        
+    }
+    res.status(401).json({message:"you dont have permisson"})
+  } catch (error) {
+    res.status(403).json({message:"token is not valid"})
+    
+  }
+
+    // const getAll = await May22Blog.find()
+    // res.send(getAll)
+}
+
+export const userGetByIdAndSignin= async (req, res) => {
+    const {email,password} = req.body
+    const hashedPassword= await bcrypt.hash(password,10)
+    const user = new May22Blog({email:email,password:hashedPassword})
+    await user.save()
+    res.send(user)
+}
+export const userGetByIdAndLogin= async (req, res) => {
+    const {email,password} = req.body
+    const user= await May22Blog.findOne({email:email})
+    if (!user) {
+        return   res.status(404).send("please enter your email")
+    }
+    const isPasswordRight = await bcrypt.compare(password, user.password)
+    if (!isPasswordRight) {
+        return res.status(403).send("sehv parol")
+        
+    }
+    const token= jwt.sign({id:user._id,email:user.email,role:user.role},tokenKey)
+     res.status(200).json({accessToken:token})
+
+
 }
 export const userGetById=  async (req, res) => {
     const { id } = req.params
@@ -13,28 +54,4 @@ export const userGetByIdAndDelete= async (req, res) => {
     const { id } = req.params
     const getDelete = await May22Blog.findByIdAndDelete(id)
     res.send(getDelete)
-}
-
-
-export const userGetByIdAndSignin= async (req, res) => {
-    const {email,password} = req.body
-    const hashedPassword= await bcrypt.hash(password,10)
-    const getAdd = new May22Blog({email,password:hashedPassword})
-    await getAdd.save()
-    res.send(getAdd)
-}
-export const userGetByIdAndLogin= async (req, res) => {
-    const {email,password} = req.body
-    const user= await May22Blog.findOne({email:email})
-    if (!user) {
-        return   res.send("not fount")
-    }
-    const isPasswordRight = await bcrypt.compare(password, user.password)
-    if (!isPasswordRight) {
-        return res.send("sehv parol")
-        
-    }
-    return res.send("xos login")
-
-
 }
--- back/src/models/UserModel.js
+++ back/src/models/UserModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const May22Schema = new mongoose.Schema({
     email: { type: String, require: true },
     password: { type: String, require: true},
+    role:{type:String,default:"User"}
    
 });
 
